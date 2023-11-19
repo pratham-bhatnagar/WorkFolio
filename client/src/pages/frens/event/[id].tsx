d214c1e2ac7761e4bb92bf26c2fd9ca0b58dba66
lakshya-dhariwal <lakshya@staq.com>
2023-11-19 12:09:38 +0530

    app structure

---
+++ client/src/pages/frens/event/[id].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div>Event control , checkin , swag distibution , user managment</div>;
+}
+
+export default Home;
---
+++ client/src/pages/host/event/[id].tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>Event desciption , apply. If applied show swag status , nft etc</div>
+  );
+}
+
+export default Home;
